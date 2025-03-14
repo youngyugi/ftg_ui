9da06ebbe01e4fa0e3c796fd253db036acbd3953
Anton Grigorita <a.grigorita00+git@gmail.com>
2025-03-14 23:38:52 +0100

    some fixes

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -14,7 +14,7 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
-const Login = async () => {
+const Login = () => {
   const [username, setUsername] = useState<string>("");
   const [password, setPassword] = useState<string>("");
   const [error, setError] = useState<boolean>(false);
