21c95c6e6a2fb2e9335b384bcf1e2db6630b6b20
Anton Grigorita <a.grigorita00+git@gmail.com>
2025-03-17 15:32:08 +0100

    added 404 - 403 - 401 erorr pages

--- app/(home)/not-found.tsx
+++ app/(home)/not-found.tsx
@@ -1,14 +1,14 @@
-const UnauthorizedPage = () => {
+const NotFound = () => {
   return (
     <main className="w-screen h-screen font-sans">
       <div className="h-screen text-center flex flex-col items-center justify-center">
         <div>
           <h1 className="inline-block mr-5 pr-6 text-2xl font-medium leading-10 align-top border-r border-slate-500">
-            401
+            404
           </h1>
           <div className="inline-block">
             <h2 className="text-sm font-normal leading-10 m-0">
-              You're not authorized to access this page.
+              This page could not be found.
             </h2>
           </div>
         </div>
@@ -17,4 +17,4 @@ const UnauthorizedPage = () => {
   );
 };
 
-export default UnauthorizedPage;
+export default NotFound;
